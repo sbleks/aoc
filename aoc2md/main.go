676699c12dafca6ccf90986160e6d999b3eb479a
Sam <31251023+sbleks@users.noreply.github.com>
2023-12-11 18:40:39 -0600

    feat: scrape a single day and create directory for it with readme and starter main.go

--- aoc2md/main.go
+++ aoc2md/main.go
@@ -4,6 +4,7 @@ import (
 	"aoc2md/scrape"
 	"fmt"
 	"log"
+	"os"
 	"strings"
 
 	md "github.com/JohannesKaufmann/html-to-markdown"
@@ -16,20 +17,37 @@ func ExampleConvert() {
 
 	converter.AddRules(
 		md.Rule{
-			Filter: []string{"del", "s", "strike"},
+			Filter: []string{"code"},
 			Replacement: func(content string, selec *goquery.Selection, opt *md.Options) *string {
 				// You need to return a pointer to a string (md.String is just a helper function).
 				// If you return nil the next function for that html element
 				// will be picked. For example you could only convert an element
 				// if it has a certain class name and fallback if not.
-				content = strings.TrimSpace(content)
-				return md.String("~" + content + "~")
+				// log.Printf("children text: %s, text: %s\n", selec.Children().Text(), selec.Text())
+				if selec.Children().Text() == selec.Text() {
+					content = strings.TrimSpace(content)
+					return md.String("_`" + selec.Text() + "`_")
+				} else {
+					return md.String("`" + content + "`")
+				}
+			},
+		},
+		md.Rule{
+			Filter: []string{"span"},
+			Replacement: func(content string, selec *goquery.Selection, opt *md.Options) *string {
+				html, err := goquery.OuterHtml(selec)
+				if err != nil {
+					return md.String(content)
+				}
+
+				return md.String(html)
 			},
 		},
 		// more rules
 	)
 
-	html := `<strike>strike me</strike>
+	html := `<div><code>I shouldn't be emphasized <em>but I should be</em></code></div>
+	<div><code><em>I should be all emphasized</em></code></div>
 	<strong>Important</strong>`
 
 	markdown, err := converter.ConvertString(html)
@@ -38,12 +56,104 @@ func ExampleConvert() {
 	}
 	fmt.Println("md ->", markdown)
 }
+
+func ConvertDay(selection *goquery.Selection) (string, error) {
+	converter := md.NewConverter("", true, nil)
+
+	converter.AddRules(
+		md.Rule{
+			Filter: []string{"code"},
+			Replacement: func(content string, selec *goquery.Selection, opt *md.Options) *string {
+				// You need to return a pointer to a string (md.String is just a helper function).
+				// If you return nil the next function for that html element
+				// will be picked. For example you could only convert an element
+				// if it has a certain class name and fallback if not.
+				// log.Printf("children text: %s, text: %s\n", selec.Children().Text(), selec.Text())
+				if selec.Children().Text() == selec.Text() {
+					content = strings.TrimSpace(content)
+					return md.String("_`" + selec.Text() + "`_")
+				} else {
+					return md.String("`" + content + "`")
+				}
+			},
+		},
+		md.Rule{
+			Filter: []string{"span"},
+			Replacement: func(content string, selec *goquery.Selection, opt *md.Options) *string {
+				html, err := goquery.OuterHtml(selec)
+				if err != nil {
+					return md.String(content)
+				}
+
+				return md.String(html)
+			},
+		},
+		// more rules
+	)
+
+	return converter.Convert(selection), nil
+
+}
+
 func main() {
-	ExampleConvert()
-	days, err := scrape.ScrapeDays("")
+	// ExampleConvert()
+	baseURL := "https://adventofcode.com"
+	year := "2023"
+	day := "4"
+	dayURL := "/" + year + "/day/" + day
+	selection, title, err := scrape.ScrapeDay(dayURL)
 	if err != nil {
-		log.Panicf("Could not scrape days: %v\n", err)
+		log.Panicf("Could not scrape day: %v\n", err)
 	}
 
-	fmt.Printf("Parsed: %v\n", days)
+	markdown, err := ConvertDay(selection)
+	if err != nil {
+		log.Panicf("Could not convert day: %v\n", err)
+	}
+
+	header := fmt.Sprintf("# %s\n\n[%s](%s)\n\n## Description\n\n### Part One\n\n", title, baseURL+dayURL, baseURL+dayURL)
+	markdown = header + markdown
+	log.Printf("Markdown:\n%s\n", markdown)
+
+	err = os.MkdirAll("../"+year+"/day"+day+"/", os.FileMode(int(0777)))
+	if err != nil {
+		log.Panicf("Could not make dir: %v\n", err)
+	}
+
+	os.Chdir("../" + year + "/day" + day + "/")
+	os.WriteFile("README.md", []byte(markdown), os.FileMode(int(0777)))
+
+	starterText := `package main
+
+import (
+	"aocInput"
+	"log"
+	"strconv"
+	"strings"
+)
+
+func part1(lines []string) (sum int) {
+	return 0
+}
+
+
+func part1(lines []string) (sum int) {
+	return 0
+}
+
+func main() {
+
+	lines, err := input.GetInputLines("./example.txt")
+	if err != nil {
+		log.Fatalf("Could not read file: %v", err)
+	}
+
+	// sum := part1(lines)
+	// sum := part2(lines)
+
+	log.Printf("Sum is: %v", sum)
+}
+`
+
+	os.WriteFile("main.go", []byte(starterText), os.FileMode(int(0777)))
 }
