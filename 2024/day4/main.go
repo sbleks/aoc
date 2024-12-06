4ea62ee5a3ba4778b4e7889777abcf0df589285c
Sam Blekhman <sam.blekhman@gmail.com>
2024-12-06 15:32:40 -0600

    wip: day 4

--- 2024/day4/main.go
+++ 2024/day4/main.go
@@ -1,23 +1,69 @@
 package main
 
 import (
-	"aocInput"
+	input "aocInput"
+	"fmt"
 	"log"
+
 	// "strconv"
-	// "strings"
+	"strings"
 )
 
-func part1(lines []string) (sum int) {
-	return sum
+type PuzzleBoard [][]rune
+
+type Puzzle struct {
+	board     PuzzleBoard
+	rowCursor int
+	colCursor int
 }
 
+func (pb PuzzleBoard) String() string {
+	rows := []string{}
+	for _, lines := range pb {
+		row := []string{}
+		for _, line := range lines {
+			row = append(row, string(line))
+		}
+		rows = append(rows, fmt.Sprintf("%v", row))
+	}
+	return strings.Join(rows, "\n")
+}
+
+func (p Puzzle) String() string {
+	return fmt.Sprintf("\n{ rowCursor: %v\ncolCursor: %v\nboard:\n%v }\n", p.rowCursor, p.colCursor, p.board)
+}
+
+func (p Puzzle) peek(row, col int) bool {
+	ch := p.board[col][row]
+
+	return false
+}
+
+func newPuzzle(lines []string) *Puzzle {
+	board := [][]rune{}
+	for _, line := range lines {
+		row := []rune{}
+		for _, ch := range line {
+			row = append(row, ch)
+		}
+		board = append(board, row)
+	}
+
+	return &Puzzle{board: board}
+}
+
+func part1(lines []string) (sum int) {
+	p := newPuzzle(lines)
+	log.Printf("%v", p)
+	return sum
+}
 
 func part2(lines []string) (sum int) {
 	return sum
 }
 
 func main() {
-	lines, err := input.GetInputLines("./input.txt")
+	lines, err := input.GetInputLines("./sample.txt")
 	if err != nil {
 		log.Fatalf("Could not read file: %v", err)
 	}
