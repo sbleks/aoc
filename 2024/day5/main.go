f2f1ed2e8f51022cde6587f123c721b9db80c706
Sam Blekhman <sam.blekhman@gmail.com>
2024-12-09 14:53:38 -0600

    wip: day 5

--- 2024/day5/main.go
+++ 2024/day5/main.go
@@ -1,29 +1,152 @@
 package main
 
 import (
-	"aocInput"
+	input "aocInput"
+	"fmt"
 	"log"
-	// "strconv"
-	// "strings"
+	"slices"
+	"strconv"
+	"strings"
 )
 
-func part1(lines []string) (sum int) {
+type Node struct {
+	before []int
+	after  []int
+}
+
+func (n Node) String() string {
+	return fmt.Sprintf("before: %v after: %v", n.before, n.after)
+}
+
+func parseInput(in string) (map[int]Node, [][]int) {
+	// Parse page ordering
+	parts := strings.Split(in, "\n\n")
+	orderInput, pageNumInput := parts[0], parts[1]
+	pageNumSlice := strings.Split(pageNumInput, "\n")
+	orders := strings.Split(orderInput, "\n")
+	graph := make(map[int]Node)
+	for _, orderPair := range orders {
+		order := strings.Split(orderPair, "|")
+		left, err := strconv.Atoi(order[0])
+		if err != nil {
+			log.Panicf("Could not parse left side of order instruction: %v", err)
+		}
+		right, err := strconv.Atoi(order[1])
+		if err != nil {
+			log.Panicf("Could not parse right side of order instruction: %v", err)
+		}
+
+		valLeft, okLeft := graph[left]
+		if okLeft {
+			if slices.Contains(valLeft.after, right) {
+				continue
+			}
+			valLeft.after = append(valLeft.after, right)
+			graph[left] = valLeft
+		} else {
+			newNode := Node{after: []int{right}}
+			graph[left] = newNode
+		}
+
+		valRight, okRight := graph[right]
+		if okRight {
+			if slices.Contains(valRight.before, left) {
+				continue
+			}
+			valRight.before = append(valRight.before, left)
+			graph[right] = valRight
+		} else {
+			newNode := Node{before: []int{left}}
+			graph[right] = newNode
+		}
+
+	}
+
+	// Parse Page Nums
+	pageNums := [][]int{}
+	for _, pageNum := range pageNumSlice {
+		// skip if empty line
+		if pageNum == "" {
+			continue
+		}
+		numStrSlice := strings.Split(pageNum, ",")
+		numSlice := []int{}
+		for _, numStr := range numStrSlice {
+			num, err := strconv.Atoi(numStr)
+			if err != nil {
+				log.Panicf("Cannot parse page num to int: %v", err)
+			}
+			numSlice = append(numSlice, num)
+		}
+
+		pageNums = append(pageNums, numSlice)
+	}
+
+	// log.Printf("%v", graph)
+
+	return graph, pageNums
+}
+
+func checkOrder(g map[int]Node, curr, next int) bool {
+	// log.Printf("curr: %v, next: %v\ng: %v", curr, next, g)
+	if slices.Contains(g[curr].after, next) {
+		return true
+	}
+
+	return false
+}
+
+func part1(in string) (sum int) {
+	graph, pageNums := parseInput(in)
+
+	for _, pageNum := range pageNums {
+		inOrder := false
+		for i := 1; i < len(pageNum); i++ {
+			inOrder = checkOrder(graph, pageNum[i-1], pageNum[i])
+			// log.Printf("%v", inOrder)
+			if !inOrder {
+				break
+			}
+		}
+		if inOrder {
+			mid := pageNum[len(pageNum)/2]
+			sum += mid
+		}
+	}
+
 	return sum
 }
 
+func part2(in string) (sum int) {
+	graph, pageNums := parseInput(in)
 
-func part2(lines []string) (sum int) {
+	for _, pageNum := range pageNums {
+		inOrder := false
+		for i := 1; i < len(pageNum); i++ {
+			inOrder = checkOrder(graph, pageNum[i-1], pageNum[i])
+			// log.Printf("%v", inOrder)
+			if !inOrder {
+				//TODO: insert sorting func here
+				break
+			}
+		}
+		if !inOrder {
+			mid := pageNum[len(pageNum)/2]
+			sum += mid
+		}
+	}
+
 	return sum
 }
 
 func main() {
-	lines, err := input.GetInputLines("./input.txt")
+	in, err := input.GetRawInput("./input.txt")
 	if err != nil {
 		log.Fatalf("Could not read file: %v", err)
 	}
 
-	sum1 := part1(lines)
-	sum2 := part2(lines)
+	sum1 := part1(in)
+	sum2 := part2(in)
 
 	log.Printf("Part 1  is: %v", sum1)
 	log.Printf("Part 2  is: %v", sum2)
